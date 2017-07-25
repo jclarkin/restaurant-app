948b44a711cb4f02f52993937b85c40931de0b4e
Jonathan Clarkin <jclarkin@tripadvisor.com>
2017-07-25 16:10:55 -0400

    Testing out some routing possibilities

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // res.send('Hey! It works!');
+  res.render('hello', { name: 'Phranklyn'});
+});
+
+router.get('/reverse/:text', (req, res) => {
+  const reversed = req.params.text.split('').reverse().join('');
+  res.json({
+    text: req.params.text,
+    reverse: reversed
+  });
 });
 
 module.exports = router;
